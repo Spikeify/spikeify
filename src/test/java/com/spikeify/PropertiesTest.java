8453266827c8ac239fe28c60ad1823cae3eb1f56
Peter Knego <peter@knego.net>
2015-04-09 15:26:48 +0200

    Fix properties test

--- src/test/java/com/spikeify/PropertiesTest.java
+++ src/test/java/com/spikeify/PropertiesTest.java
@@ -1,43 +1,45 @@
-//package com.spikeify;
-//
-//import org.junit.Assert;
-//import org.junit.Test;
-//
-//import java.util.Map;
-//
-//public class PropertiesTest {
-//
-//	@Test
-//	public void loadProps() {
-//
-//		EntityOne entityLoad = new EntityOne();
-//		entityLoad.one = 123;
-//		entityLoad.two = "a test";
-//		entityLoad.three = 123.0d;
-//		entityLoad.four = 123.0f;
-//		entityLoad.setFive((short) 234);
-//		entityLoad.setSix((byte) 100);
-//		entityLoad.ignored = "should be ignored";
-//
-//		ClassMapper mapper = new ClassMapper(EntityOne.class);
-//		Map<String, Object> props = mapper.getProperties(entityLoad);
-//
-//		Assert.assertEquals(123, props.get("one"));
-//		Assert.assertEquals("a test", props.get("two"));
-//		Assert.assertEquals(123.0d, props.get("three"));
-//		Assert.assertEquals(123.0f, props.get("four"));
-//		Assert.assertEquals((short) 234, props.get("five"));
-//		Assert.assertEquals((byte) 100, props.get("six"));
-//		Assert.assertFalse(props.containsKey("ignore"));
-//
-//		EntityOne entitySave = new EntityOne();
-//		mapper.setFieldValues(entitySave, props);
-//
-//		// ignored field was not saved
-//		Assert.assertNull(entitySave.ignored);
-//
-//		// set ignored field and compare with original
-//		entitySave.ignored = "should be ignored";
-//		Assert.assertEquals(entityLoad, entitySave);
-//	}
-//}
+package com.spikeify;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Map;
+
+public class PropertiesTest {
+
+	@Test
+	public void loadProps() {
+
+		EntityOne entityLoad = new EntityOne();
+		entityLoad.one = 123;
+		entityLoad.two = "a test";
+		entityLoad.three = 123.0d;
+		entityLoad.four = 123.0f;
+		entityLoad.setFive((short) 234);
+		entityLoad.setSix((byte) 100);
+		entityLoad.ignored = "should be ignored";
+
+		ClassMapper mapper = new ClassMapper(EntityOne.class);
+		Map<String, Object> props = mapper.getProperties(entityLoad);
+
+		// database saves & returns everything as long, string or byte array
+		// we need to convert accordingly
+		Assert.assertEquals(123, ((Long) props.get("one")).intValue());
+		Assert.assertEquals("a test", props.get("two"));
+		Assert.assertEquals(123.0d, Double.longBitsToDouble((long) props.get("three")), 0.1);
+		Assert.assertEquals(123.0f, (float) Double.longBitsToDouble((long) props.get("four")), 0.1);
+		Assert.assertEquals((short) 234, Long.valueOf((long) props.get("five")).shortValue());
+		Assert.assertEquals((byte) 100, Long.valueOf((long) props.get("six")).byteValue());
+		Assert.assertFalse(props.containsKey("ignore"));
+
+		EntityOne entitySave = new EntityOne();
+		mapper.setFieldValues(entitySave, props);
+
+		// ignored field was not saved
+		Assert.assertNull(entitySave.ignored);
+
+		// set ignored field and compare with original
+		entitySave.ignored = "should be ignored";
+		Assert.assertEquals(entityLoad, entitySave);
+	}
+}
