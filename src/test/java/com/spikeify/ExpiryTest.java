7c35092d9fcd948f6a8d8f64e445d35739afe9f4
romanorac <orac.roman@gmail.com>
2016-01-20 17:05:26 +0100

    expire test problem

--- src/test/java/com/spikeify/ExpiryTest.java
+++ src/test/java/com/spikeify/ExpiryTest.java
@@ -87,4 +87,30 @@ public class ExpiryTest {
 		Assert.assertEquals(futureDate, reloaded.expires, 5000);
 	}
 
+	/**
+	 * Set relative expiration date problem. Test is successful, but record will never expire.
+	 *
+	 * expire value is mapped correctly by Spikeify as 86400000.
+	 * But Spikeify saves the expire value in AS as -1453219341 and AS logs following warning:
+	 * WARNING (rw): (thr_rw.c::3136) {test} ttl 2841748150 exceeds 315360000 - set config value max-ttl to suppress this warning <Digest>:0x5da19e0a4e90067b2eada5a63afbfd21a71c44b4
+	 * So record will never expire.
+	 *
+	 */
+	@Test
+	public void setExpiresRelative() {
+		EntityExpires entity = new EntityExpires();
+
+		long relativeDate = 24L * 60L * 60L * 1000L; //expire in one day
+		entity.expires = relativeDate;
+		Key key1 = new Key(namespace, setName, userKey1);
+
+		Key saveKey = sfy
+				.update(key1, entity)
+				.now();
+
+		EntityExpires reloaded = sfy.get(EntityExpires.class).key(saveKey).now();
+		Assert.assertEquals(relativeDate, reloaded.expires, 5000); //test successed
+
+	}
+
 }
